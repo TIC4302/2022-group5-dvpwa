28c2818ee13965182db244cc3f6ff3f4a1c6ecf2
Nuri-Irfani <cranberrywalnut7@gmail.com>
2022-04-24 13:33:33 +0000

    updated password hashing

--- sqli/dao/user.py
+++ sqli/dao/user.py
@@ -40,6 +40,6 @@ class User(NamedTuple):
     def check_password(self, password: str):
         bytePwd = password.encode('utf-8')
         mySalt = bcrypt.gensalt()
-        self.pwd_hash == bcrypt.hashpw(bytePwd, mySalt)
+        self.pwd_hash = bcrypt.hashpw(bytePwd, mySalt)
         return self.pwd_hash == bcrypt.checkpw(password,self.pwd_hash)
         #return self.pwd_hash == md5(password.encode('utf-8')).hexdigest()
